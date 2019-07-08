8015e71aad4bffddc11d92e5d32884b01f777769
raimoainla <raimo.ainla@marketingclinic.fi>
2019-07-08 13:16:12 +0300

    handled default search keyword, rendered default items, dealt with react warnings

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 constructor(props) {
   super(props);
-  this.state = {videos: []};
+  this.state = {
+    videos: [],
+    selectedVideo: null
+  };
+}
+
+componentDidMount() {
+  this.onInputSubmit('buildings');
 }
 
 onInputSubmit = async (input) => {
@@ -16,15 +24,31 @@ onInputSubmit = async (input) => {
       q:input
     }
   });
-  this.setState({videos: response.data.items});
+  this.setState({
+    videos: response.data.items,
+    selectedVideo: response.data.items[0]
+  });
+};
+
+onVideoSelect = (video) => {
+  this.setState({selectedVideo:video});
 };
 
 render () {
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={this.onInputSubmit}/>
-      <VideoList videos={this.state.videos}/>
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={this.state.selectedVideo}/>
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 }
